04fe872aff5638f2601961d83d0ba970130e0150
Courtney Trudel <co.trudel@gmail.com>
2020-06-19 13:02:12 -0400

    "numbered" map layer now fully populated, map component pulls data from json file, city markers operational

--- resources/js/components/admin/MapComponent.vue
+++ resources/js/components/admin/MapComponent.vue
@@ -14,11 +14,12 @@
                     :bounds="bounds"
             />
             <l-marker
-                    v-for="star in stars"
-                    :key="star.name"
-                    :lat-lng="star"
+                    v-for="city in mapData.cities"
+                    :key="city.name"
+                    :lat-lng="city"
+                    :icon="newIcon('city')"
             >
-            <l-popup :content="star.name" />
+            <l-popup :content="city.name" />
             </l-marker>
         </l-map>
     </div>
@@ -27,6 +28,7 @@
 <script>
     import { CRS } from "leaflet";
     import { LMap, LImageOverlay, LMarker, LPopup, LPolyline } from "vue2-leaflet";
+    import iconData from "../../json/icons.json";
 
     export default {
         components: {
@@ -44,23 +46,49 @@
                 maxBounds: bounds,
                 maxBoundsViscosity: 1.0,
                 minZoom: -2,
-                maxZoom: 2,
+                maxZoom: 1,
                 crs: CRS.Simple,
-                stars: [
-                        { name: "Sol", lng: 175.2, lat: 145.0 },
-                        { name: "Mizar", lng: 41.6, lat: 130.1 },
-                        { name: "Krueger-Z", lng: 13.4, lat: 56.5 },
-                        { name: "Deneb", lng: 218.7, lat: 8.3 }
-                    ]
+                map: null,
+                mapData: [],
+                icons: []
             };
         },
+        created: function () {
+            // import demo data
+            var _this = this;
+            $.getJSON("/resources/demoMap.json", function (json) {
+                _this.mapData = json;
+            });
+        },
         mounted() {
-            var map = this.$refs.map.mapObject;
+            this.setupVars();
+
+            // create overlay layers
             var overlayLayers = {
-                'Numbered':L.imageOverlay("/images/kingmaker-map-numbered.png", this.bounds)
+                'Hex Identifiers':L.imageOverlay("/images/kingmaker-map-numbered.png", this.bounds)
             };
-            L.control.layers(null, overlayLayers).addTo(map);
-            map.setView([-50, 450], .10);
+            L.control.layers(null, overlayLayers).addTo(this.map);
+
+            // set initial map view
+            this.map.setView([-50, 450], .10);
+        },
+        methods: {
+            setupVars: function () {
+                // set up reference to component
+                var _this = this;
+
+                // set map object and store in data
+                this.map = this.$refs.map.mapObject;
+
+                // create icon types
+                Object.keys(iconData).forEach(iconKey => {
+                    _this.icons[iconKey] = L.Icon.extend(iconData[iconKey]);
+                })
+            },
+            newIcon: function (type) {
+                // construct new icon of type
+                return new this.icons[type];
+            }
         }
     };
 </script>
